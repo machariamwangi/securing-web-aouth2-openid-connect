82a717a97cc30dd0cbf9f0732e1fa9613f3c25c6
ammwangi <elijah.mwangi@kamsware.com>
2022-02-24 09:19:08 +0300

    authguard and interceptors

---
+++ src/app/core/admin.route-guard.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from './auth-service.component';
+
+@Injectable()
+export class AdminRouteGuard implements CanActivate {
+    constructor(private _authService: AuthService) { }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        return  !!this._authService.authContext && this._authService.authContext.isAdmin;
+    }
+}
